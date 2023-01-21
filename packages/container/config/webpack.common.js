dbf386523827afdf04c34e61737299fa05c4df65
Rakesh Cheekatimala <rakeshcheekatimala@gmail.com>
2023-01-21 23:50:19 +0800

    :fire: refactor the common config as per production

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HTMLWebpackPugin = require("html-webpack-plugin");
+
 module.exports = {
   module: {
     rules: [
@@ -5,13 +7,18 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime'],
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
           },
         },
       },
     ],
   },
+  plugins: [
+    new HTMLWebpackPugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
