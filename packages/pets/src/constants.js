eabd4689bafa1b27e70b6352c0a694a19a247c7d
Rakesh Cheekatimala <rakeshcheekatimala@gmail.com>
2023-01-26 14:40:09 +0800

    Update constants.js

--- packages/pets/src/constants.js
+++ packages/pets/src/constants.js
@@ -1,2 +1,2 @@
-export const PETS_API_URL = 'http://pets-v2.dev-apis.com/pets';
-export const BREEDS_API_URL = 'http://pets-v2.dev-apis.com/breeds';
+export const PETS_API_URL = 'https://pets-v2.dev-apis.com/pets';
+export const BREEDS_API_URL = 'https://pets-v2.dev-apis.com/breeds';
